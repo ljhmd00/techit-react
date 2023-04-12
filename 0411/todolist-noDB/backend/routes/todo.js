efb675b3c1c272f8dbd287fae397ebd3df60eab6
ljhmd00 <lmsmd00@naver.com>
2023-04-12 09:34:56 +0900

    수정

--- 0411/todolist-noDB/backend/routes/todo.js
+++ 0411/todolist-noDB/backend/routes/todo.js
@@ -4,33 +4,40 @@ let todoData = require("../todoData.json");
 
 const router = express.Router();
 
-//전체 투두리스트 조회
+//  전체 투두리스트 조회
 router.get("/", (req, res) => {
     console.log(todoData);
 
-    res.json(todoData[0].title);
+    res.json(todoData);
 });
 
-//특정 투두리스트 조회
+// 특정 투두리스트 조회
 router.get("/:id", (req, res) => {
     const { id } = req.params;
+
     if (parseInt(id) >= todoData.length) {
-        res.send({ error: "존재하지 않는 ID입니다." });
+        return res.status(400).json({ error: "존재하지 않는 ID입니다." });
     }
 
     res.json(todoData[parseInt(id)]);
 });
 
-//투두 생성
+// 투두 생성
 router.post("/", (req, res) => {
     const { title, desc } = req.body;
 
-    console.log(title, desc);
-    todoData.push({ title, desc });
+    if (!title || !desc) {
+        return res.status(400).json({ error: "타이틀과 설명을 입력하셔야 합니다." });
+    }
+
+    todoData.push({ title, desc, isDone: false });
+
+    console.log(todoData);
+
     res.json(todoData);
 });
 
-//특정 투두리스트 업데이트
+// 투두 수정
 router.put("/:id", (req, res) => {
     const { id } = req.params;
     const { title, desc } = req.body;
@@ -39,7 +46,7 @@ router.put("/:id", (req, res) => {
         return res.status(400).json({ error: "존재하지 않는 ID입니다." });
     }
     if (!title && !desc) {
-        res.status(400).json({ error: "타이틀이나 설명 중에 하나의 값은 입력해야 합니다." });
+        return res.status(400).json({ error: "타이틀이나 설명 중에 하나의 값은 입력해야 합니다." });
     }
 
     todoData[parseInt(id)] = {
@@ -49,12 +56,13 @@ router.put("/:id", (req, res) => {
     };
 
     console.log(todoData);
+
     res.json(todoData);
 });
 
-//특정 투두리스트 업데이트 완료
+// 투두 완료 처리
 router.put("/done/:id", (req, res) => {
-    const { id } = req.params; // id값
+    const { id } = req.params;
 
     if (parseInt(id) >= todoData.length) {
         return res.status(400).json({ error: "존재하지 않는 ID입니다." });
@@ -65,12 +73,13 @@ router.put("/done/:id", (req, res) => {
         desc: todoData[parseInt(id)].desc,
         isDone: !todoData[parseInt(id)].isDone,
     };
+
     console.log(todoData);
 
     res.json(todoData);
 });
 
-//특정 투두리스트 삭제
+// 투두 삭제
 router.delete("/:id", (req, res) => {
     const { id } = req.params;
 
@@ -83,6 +92,7 @@ router.delete("/:id", (req, res) => {
     });
 
     console.log(todoData);
+
     res.json(todoData);
 });
 
