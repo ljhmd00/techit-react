b46a68ef5d31e6fb9dcbc8d74eb6f4ee85cd785e
ljhmd00 <lmsmd00@naver.com>
2023-04-13 09:20:43 +0900

    0413 todolist 복습

---
+++ 0412/todolist-noDB/backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const todoRouter = require("./routes/todo");
+
+const app = express();
+
+const port = 3030;
+
+app.use(cors());
+app.use(express.json());
+app.use("/todo", todoRouter);
+
+app.get("/", (req, res) => {
+    res.send("Hello, Express");
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}🎉`);
+});
