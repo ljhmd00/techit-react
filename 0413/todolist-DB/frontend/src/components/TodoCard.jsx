952fc486eaac45bb08cced34bd6d9587337268f5
ljhmd00 <lmsmd00@naver.com>
2023-04-17 13:26:39 +0900

    feat: TodoCard

---
+++ 0413/todolist-DB/frontend/src/components/TodoCard.jsx
@@ -0,0 +1,19 @@
+const TodoCard = ({ todo, isDone }) => {
+    return (
+        <div className="flex my-4">
+            {isDone ? (
+                <>
+                    <div className="relative">
+                        <div className="border-4 border-pink-400 w-8 h-8 rounded-xl bg-pink-400 p-2"></div>
+                        <div className="absolute border-4 border-white top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-8 h-8 scale-75 rounded-xl bg-pink-400 p-2"></div>
+                    </div>
+                </>
+            ) : (
+                <div className="border-4 border-pink-400 w-8 h-8 rounded-xl"></div>
+            )}
+            <div className="text-2xl ml-4 truncate">{todo}</div>
+        </div>
+    );
+};
+
+export default TodoCard;
