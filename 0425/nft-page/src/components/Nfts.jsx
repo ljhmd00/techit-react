fa05fa2170131eab0c8d719d0e45b0f80209ed7e
ljhmd00 <lmsmd00@naver.com>
2023-04-26 13:17:30 +0900

    feat: Nfts.jsx

--- 0425/nft-page/src/components/Nfts.jsx
+++ 0425/nft-page/src/components/Nfts.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import NftCard from "./NftCard";
 
-const Nfts = ({ page }) => {
+const Nfts = ({ page, mintedNft }) => {
     const [selectedPage, setSelectedPage] = useState(1);
     const [nfts, setNfts] = useState();
 
     const getNfts = async (p) => {
         try {
             let nftArray = [];
+
+            setNfts();
 
             for (let i = 0; i < 10; i++) {
                 const tokenId = i + 1 + (p - 1) * 10;
@@ -25,6 +28,7 @@ const Nfts = ({ page }) => {
 
     const onCLickPage = (p) => () => {
         setSelectedPage(p);
+        getNfts(p);
     };
 
     const pageComp = () => {
@@ -59,8 +63,24 @@ const Nfts = ({ page }) => {
     }, []);
 
     return (
-        <div>
-            <div className="text-center">{pageComp()}</div>
+        <div className="max-w-screen-xl mx-auto">
+            <div>{pageComp()}</div>
+            <ul className="mt-8 grid gird-cols-1 xl:grid-cols-2 justify-items-center gap-8">
+                {nfts ? (
+                    nfts.map((v, i) => {
+                        return (
+                            <NftCard
+                                key={i}
+                                tokenId={v.tokenId}
+                                metadata={v.metadata}
+                                mintedNft={mintedNft}
+                            />
+                        );
+                    })
+                ) : (
+                    <div>로딩중입니다...</div>
+                )}
+            </ul>
         </div>
     );
 };
